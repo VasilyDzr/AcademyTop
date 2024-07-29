31c8dc1c63cf46b3b3da3b5b3ec7e5754f9f2190
VasilyDzr <vsily@list.ru>
2024-07-29 21:15:03 +0300

    Add files via upload

--- SeaBattle/impl.py
+++ SeaBattle/impl.py
@@ -1,4 +1,10 @@
 def init_fields(fields_number, side):
+    """
+    Создание основы полей морского боя
+    :param fields_number: количество полей
+    :param side: размеры стороны поля (квадратного)
+    :return: подготовленное поле для заполнения
+    """
     fields = []
     for k in range(fields_number):
         fields.append([])
@@ -10,6 +16,11 @@ def init_fields(fields_number, side):
     return fields
 
 def draw_fields(fields):
+    """
+    Заполенение поля символами
+    :param fields:
+    :return:
+    """
     SPASE_FIELDS = 4
     SPASE_CELLSE = 3
     print('   ', end='')
@@ -17,9 +28,13 @@ def draw_fields(fields):
         for j in range(len(fields[0])):
 
             _, tmp = coord_atou(0, j)
-            print(tmp, end='')
-            print(' ' * SPASE_CELLSE, end='')
-        print(' ' * (SPASE_FIELDS + 2), end='')
+            if tmp < 10:
+                print(tmp, end='')
+                print(' ' * SPASE_CELLSE, end='')
+            else:
+                print(tmp, end='')
+                print(' ' * (SPASE_CELLSE-1), end='')
+        print(' ' * (SPASE_FIELDS + 3), end='')
     print()
 
     for i in range(len(fields[0])):
@@ -34,6 +49,11 @@ def draw_fields(fields):
         print('')
 
 def get_cell_simbul(value):
+    """
+    Замена символов в ячейке на заданный для данного типа корабля
+    :param value:
+    :return:
+    """
     if value == 0:
         return '.'
     elif value == 1:
@@ -44,11 +64,23 @@ def get_cell_simbul(value):
         return '3'
     elif value == 4:
         return '4'
+    elif value == -1:
+        return 'X'
+    elif value == 'Y':
+        return 'Y'
+    elif value == 'N':
+        return 'N'
     else:
         return '*'
 
 def coord_utoa(vert, horiz):
-    j = horiz - 1
+    """
+    Перевод координат пользовательских на програмные (A 2 >> 0 1)
+    :param vert: координаты вертикальные
+    :param horiz: координаты горизонтальные
+    :return:
+    """
+    j = int(horiz) - 1
     tmp = {
         'А': 0,
         'Б': 1,
@@ -60,19 +92,128 @@ def coord_utoa(vert, horiz):
         'З': 7,
         'И': 8,
         'К': 9,
+        'Л': 10,
+        'М': 11,
+        'Н': 12,
+        'О': 13,
+        'П': 14,
+        'Р': 15,
+        'С': 16,
+        'Т': 17,
     }
     i = tmp[vert]
     return (i, j)
 
 def coord_atou(i, j):
-    s = 'АБВГДЕЖЗИК'
+    """
+    Перевод координат програмных на пользовательские (0 1 >> A 2)
+    :param i: координаты вертикальные
+    :param j: координаты горизонтальные
+    :return:
+    """
+    s = 'АБВГДЕЖЗИКЛМНОПРСТ'
     vert = s[i]
     horiz = j + 1
     return (vert, horiz)
 
-def add_ship(field: list, ship_len: int, head_coord: tuple, is_horizontal: bool) -> bool:
+def add_ship(fields: list, ship_len: int, head_coord: list, is_horizontal: bool) -> bool:
+    """
+    Добаление корабля на поле, если корабль не выходит за границы поля
+    :param field: номер поля
+    :param ship_len: длина корабля
+    :param head_coord: начальная точка в програмных координатах
+    :param is_horizontal: вертикальный / горизонтальный корабль
+    :return: Получилось добавить / не получилось
+    """
+    vert, horiz = coord_utoa(head_coord[0], head_coord[1])
+    if horiz + ship_len > len((fields[0])) or vert + ship_len > len((fields[0])):
+        print('Корабль выходит за игровое поле')
+        return False
+    else:
+        # Меняет символ корабля на нужный
+        for i in range(ship_len):
+            fields[vert][horiz] = ship_len
+            get_cell_simbul(ship_len)
+            if is_horizontal:
+                horiz += 1
+            else:
+                vert += 1
+        # Окружает корабль полем Х (понятно, что сделанно криво, но не понятно, как сделать по нормальному)
+        # vert, horiz = coord_utoa(head_coord[0], head_coord[1])
+        # if is_horizontal:
+        #
+        #     # Когда корабль не прижат к стенкам
+        #     if vert != 0 and vert != len((fields[0])) and horiz != 0 and horiz != len((fields[0]))-1:
+        #         for i in range(ship_len):
+        #             fields[vert - 1][horiz] = -1
+        #             fields[vert + 1][horiz] = -1
+        #             horiz += 1
+        #         for i in range(3):
+        #             fields[vert - 1][horiz-ship_len-1] = -1
+        #             fields[vert - 1][horiz] = -1
+        #             vert += 1
+        #
+        #     # Когда корабль прижат к верхнему краю поля, но не в углу
+        #     elif vert == 0 and horiz != 0 and horiz != len((fields[0])) and horiz != 0:
+        #         for i in range(ship_len):
+        #             fields[vert + 1][horiz - ship_len] = -1
+        #             horiz += 1
+        #         for i in range(2):
+        #             fields[vert][horiz - ship_len] = -1
+        #             fields[vert][horiz - ship_len * 2 - 1] = -1
+        #             vert += 1
+        #
+        #     # Когда корабль прижат к нижнему краю поля, но не в углу
+        #     elif vert == len((fields[0]))-1 and horiz != 0 and horiz != len((fields[0]))-1:
+        #         for i in range(ship_len):
+        #             fields[vert - 1][horiz - ship_len] = -1
+        #
+        #             horiz += 1
+        #         for i in range(2):
+        #             fields[vert - 1][horiz - ship_len] = -1
+        #             fields[vert - 1][horiz - ship_len * 2 - 1] = -1
+        #             vert += 1
+        #
+        #     # Когда корабль прижат к правой стенки
+        #     elif vert != 0 and vert != len((fields[0])) and horiz == len((fields[0]))-1:
+        #         for i in range(ship_len):
+        #             fields[vert - 1][len((fields[0]))-1] = -1
+        #             fields[vert + 1][len((fields[0]))-1] = -1
+        #             horiz += 1
+        #         for i in range(3):
+        #             fields[vert - 1][len((fields[0]))-2] = -1
+        #             vert += 1
+        #
+        #     # Когда корабль прижат к левой стенки
+        #     elif vert != 0 and vert != len((fields[0])) and horiz == 0:
+        #         for i in range(ship_len):
+        #             fields[vert - 1][horiz] = -1
+        #             fields[vert + 1][horiz] = -1
+        #             horiz += 1
+        #         for i in range(3):
+        #             fields[vert - 1][horiz] = -1
+        #             vert += 1
+        #
+        #     # Когда корабль прижат к верхн. прав. углу
+        #     elif vert == 0 and horiz == 0:
+        #         for i in range(ship_len):
+        #             fields[vert + 1][horiz] = -1
+        #             horiz += 1
+        #         for i in range(2):
+        #             fields[vert ][horiz] = -1
+        #             vert += 1
 
 
 
+    return True
 
-    return False
+def attak_ship(field, attak_coord):
+    vert, horiz = coord_utoa(attak_coord[0], attak_coord[1])
+    if field[vert][horiz] == 1 or field[vert][horiz] == 2 or field[vert][horiz] == 3 or field[vert][horiz] == 4:
+        field[vert][horiz] = 'Y'
+        get_cell_simbul(field[vert][horiz])
+        print('Вы попали')
+    else:
+        field[vert][horiz] = 'N'
+        get_cell_simbul(field[vert][horiz])
+        print('Вы не попали')
--- SeaBattle/main.py
+++ SeaBattle/main.py
@@ -1,23 +1,87 @@
 from impl import *
 
-N = 10
+N = 10 # Размер поля
+Gamer = 2
 
-fields = init_fields(2, N)
+fields = init_fields(Gamer, N)
 
-# fields[0][3][5] = 1
+# Ручная расстановка кораблей (сделано для поля 10Х10 и стандартного набора кораблей)
 
-# i, j = coord_utoa('А', 2)
-#
-# vert, horiz = coord_atou(0, 1)
+for gamer in range(Gamer):
+    print('Игрок', gamer+1, 'расставляет корабли')
+    # Добавление одноклеточных кораблей
 
 
-# print(i, j, vert, horiz)
+    for boat in range(4):
+        head_coord_v = input(f'Введите координаты однопалубного №{boat+1}:')
+        head_coord = []
+        head_coord.append(head_coord_v[0])
+        head_coord.append(head_coord_v[1::])
+        print(head_coord)
+        is_success = add_ship(fields[gamer], 1, head_coord, True)
+    # Добавление двух-клеточных кораблей
+    for boat_s in range(3):
+        head_coord_v = input(f'Введите координаты двухпалубного №{boat_s+1}:')
+        head_coord = []
+        head_coord.append(head_coord_v[0])
+        head_coord.append(head_coord_v[1::])
+        is_horizontal = input(f'Горизонтальный (Г) или вертикальный (В):')
+        if is_horizontal == 'Г':
+            is_horizontal = True
+        elif is_horizontal == 'В':
+            is_horizontal = False
+        is_success = add_ship(fields[gamer], 2, head_coord, is_horizontal)
+    # Добавление трех-клеточных кораблей
+    for boat_t in range(2):
+        head_coord_v = input(f'Введите координаты трехпалубного №{boat_t + 1}:')
+        head_coord = []
+        head_coord.append(head_coord_v[0])
+        head_coord.append(head_coord_v[1::])
+        is_horizontal = input(f'Горизонтальный (Г) или вертикальный (В):')
+        if is_horizontal == 'Г':
+            is_horizontal = True
+        elif is_horizontal == 'В':
+            is_horizontal = False
+        is_success = add_ship(fields[gamer], 3, head_coord, is_horizontal)
+    # Добавление четырех-клеточных кораблей
+    for boat_f in range(1):
+        head_coord_v = input(f'Введите координаты четырехпалубного №{boat_f + 1}:')
+        head_coord = []
+        head_coord.append(head_coord_v[0])
+        head_coord.append(head_coord_v[1::])
+        is_horizontal = input(f'Горизонтальный (Г) или вертикальный (В):')
+        if is_horizontal == 'Г':
+            is_horizontal = True
+        elif is_horizontal == 'В':
+            is_horizontal = False
+        is_success = add_ship(fields[gamer], 4, head_coord, is_horizontal)
 
 # Добавим корабль (0 поле, длина кор., коорд начала, направление: False(вертикальный))
 
-is_success = add_ship(fields[0], 2, ('А', 2), False)
+# is_success = add_ship(fields[0], 1, ('А', 2), True)
 
 if is_success:
     print('Корабль добавлен')
 
 draw_fields(fields)
+
+
+# Атака по корабликам
+while True:
+    for gamer in range(Gamer):
+        print('Игрок', gamer+1, 'атакует')
+
+        attak_coord_v = input(f'Введите координаты атаки:')
+        attak_coord = []
+        attak_coord.append(attak_coord_v[0])
+        attak_coord.append(attak_coord_v[1::])
+        if gamer + 1 < Gamer:
+            attak_ship(fields[gamer+1], attak_coord)
+        else:
+            gamer = -1
+            attak_ship(fields[gamer + 1], attak_coord)
+        draw_fields(fields)
+
+# head_coord = tuple(input(f'Введите координаты однопалубного №{boat+1}:'))
+
+# draw_fields(fields)
